702578e86e04b8cd6201f41b17e47aeb65fc28ff
HTSpankaj <109585437+HTSpankaj@users.noreply.github.com>
2022-10-04 18:28:35 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   swcMinify: true,
-  images: {
-    unoptimized: true,
-  },
 }
 
 module.exports = nextConfig
